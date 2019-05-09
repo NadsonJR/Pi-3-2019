cfe4f50e5d3f5fac15ff0c1e96eeb570e01cc5cf
nadso <nadso@192.168.15.36>
2019-05-09 17:17:59 -0300

    Filial Função Excluir Funcionando

---
+++ mavenproject1/pi-2019/src/main/java/SERVLETS/FilialExcluir.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SERVLETS;
+
+import DAO.FilialDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nadso
+ */
+@WebServlet(name = "FilialExcluir", urlPatterns = {"/FilialExcluir"})
+public class FilialExcluir extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("JSP-PAGES/ConsultaFilial.jsp");
+        dispatcher.forward(request, response);
+    
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int IDFilial = Integer.parseInt(request.getParameter("id"));
+        try {
+            if(FilialDAO.delFilial(IDFilial)){
+                request.setAttribute("msgResposta", "Excluido com sucesso!");
+            } else {
+                request.setAttribute("msgResposta", "Não Foi possível realizar a exclusão!");
+            }
+        } catch (Exception e) {
+            e.getLocalizedMessage();
+            System.out.println(e);
+        }
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("JSP-PAGES/Home.jsp");
+        dispatcher.forward(request, response);
+    }
+}
